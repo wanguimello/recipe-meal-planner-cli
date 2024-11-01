1624d600b7578ebaff081917ef9cedf0e085bc71
joycewangui <joycewangui590@gmail.com>
2024-11-01 09:17:49 +0300

    Adds content to ingredients

--- src/models/ingredient.py
+++ src/models/ingredient.py
@@ -1,8 +1,18 @@
-from sqlalchemy import Column, Integer, String
-from . import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database.setup import Base
 
 class Ingredient(Base):
-    __tablename__ = "ingredients"
+    __tablename__ = 'ingredients'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    quantity = Column(String, nullable=False)
+    unit = Column(String, nullable=False)
+
+    # Foreign key to reference Recipe
+    recipe_id = Column(Integer, ForeignKey('recipes.id'))
+    recipe = relationship('Recipe', back_populates='ingredients')
+
+    def __repr__(self):
+        return f"<Ingredient(name={self.name}, quantity={self.quantity} {self.unit})>"
