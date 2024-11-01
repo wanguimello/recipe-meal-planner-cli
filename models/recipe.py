2c5d6d3cf90d0c16c6c94e014a27edccfe822d07
joycewangui <joycewangui590@gmail.com>
2024-11-01 08:47:32 +0300

    Adding the recipe function to collect user input and save recipe database

---
+++ models/recipe.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    instructions = Column(String)
+    cooking_time = Column(Integer)
+    difficulty = Column(String)
+
+    ingredients = relationship("Ingredient", secondary="recipe_ingredients")
